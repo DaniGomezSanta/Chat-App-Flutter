8498108f2c3a208aa91066348a36959121a57179
Daniela Gomez <ledangom@gmail.com>
2023-05-03 19:00:49 -0500

    first commit

---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage('https://static.wikia.nocookie.net/doblaje/images/e/ed/ZaynMalik.jpg/revision/latest?cb=20221126070912&path-prefix=es'),
+          ),
+        ),
+        title: const Text('Mi amor'),
+        centerTitle: false,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                return ( index %  2 == 0)
+                ? const HerMessageBubble()
+                : const MyMessageBubble();
+              })),
+
+
+              //caja de texto de mensajes 
+              const MessageFieldBox(),
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
